a4dfeabc4278bea08af5c70bef891f89a9444210
hayla <hmr9162@nyu.edu>
2026-08-11 13:06:43 -0400

    Create app.js
    add open function (borrowed from Shredder 1.0 html)

---
+++ docs/app.js
@@ -0,0 +1,9 @@
+// zim sum app.js
+
+// open home site from button click 
+
+function openZIMSUM() {
+   var zimWin = window.open("/ZimSum/index.html","",'width=900,height=675,resizable=yes,menubar=no,toolbar=no,status=yes,scrollbars=yes,top=10,left=10');
+   zimWin.focus();
+
+
