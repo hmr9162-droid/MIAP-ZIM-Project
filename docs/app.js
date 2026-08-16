69e8664820f4daa7c5befede961c06716a732a7f
hayla <hmr9162@nyu.edu>
2026-08-16 14:02:28 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -6,4 +6,20 @@ function openZIMSUM() {
    var zimWin = window.open("/ZimSum/index.html","",'width=900,height=675,resizable=yes,menubar=no,toolbar=no,status=yes,scrollbars=yes,top=10,left=10');
    zimWin.focus();
 
+// leave file folder "full" after download button is clicked
+   
+document.querySelectorAll(".download-folder").forEach(folder => {
+    folder.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const link = this;
+
+        link.classList.add("clicked");
+
+        setTimeout(() => {
+            window.location.href = link.href;
+        }, 300);
+    });
+});
+
 
